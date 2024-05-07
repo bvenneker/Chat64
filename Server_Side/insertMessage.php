c0fe5e9dbad74796865eb960a219bb9614c87785
Bart Venneker <b.venneker@gmail.com>
2024-05-07 11:44:39 +0200

    Update insertMessage.php
    php code is now aware if cartrdige retries to send a message. This prevents double inserts

--- Server_Side/insertMessage.php
+++ Server_Side/insertMessage.php
@@ -1,5 +1,6 @@
 <?php
 //Sven Pook v2
+//may 7, 2024 BV added retry count
 
 
 require_once('../dbCredent.php');
@@ -13,28 +14,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Sanitize input data
     $regid = test_input($_POST["regid"]);
-    $call = test_input($_POST["call"]);
-    $sendername = test_input($_POST["sendername"]);
-    $recipientname = test_input($_POST["recipientname"]);
-    $recipientname = mb_convert_case($recipientname, MB_CASE_TITLE, "UTF-8");
-    $message = test_input($_POST["message"]);
-
-    // Decode base64 encoded message
-    $message = base64_decode($message);
-
-    // Escape special characters to prevent SQL injection
-    $sendername = $conn->real_escape_string($sendername);
-    $recipientname = $conn->real_escape_string($recipientname);
-    $message = $conn->real_escape_string($message);
-
-    // delete the last char if it is not printable
-	if (ctype_print(substr($message, -1))==False){
-		$message = substr_replace($message, '', -1);
-		}
+    if (isset($_POST["call"])) $call = test_input($_POST["call"]);
     
-    // Prepare recipient ID
-    $recipientID = "";
-
     if ($call == "heartbeat") {
         // Update last seen field for the user
         $sql = "UPDATE users SET lastseen = " . time() . " WHERE regid = '$regid'";
@@ -46,8 +27,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $conn->close();
         exit();
     }
+    
+    $retryCount=0;
+    if (isset($_POST["retryCount"])) $retryCount = test_input($_POST["retryCount"]); 
+    if (isset($_POST["sendername"])) $sendername = test_input($_POST["sendername"]);    
+    if (isset($_POST["recipientname"])) { 
+		$recipientname = test_input($_POST["recipientname"]);
+		$recipientname = mb_convert_case($recipientname, MB_CASE_TITLE, "UTF-8");
+	}
+    $message = test_input($_POST["message"]);
+
+    // Decode base64 encoded message
+    $message = base64_decode($message);
+	
+    // delete the last char if it is not printable
+    if (ctype_print(substr($message, -1))==False){
+        $message = substr_replace($message, '', -1);
+    }
+		
+    // Escape special characters to prevent SQL injection
+    $sendername = $conn->real_escape_string($sendername);
+    $recipientname = $conn->real_escape_string($recipientname);
+    $message = $conn->real_escape_string($message);
+
+    // Prepare recipient ID
+    $recipientID = "";
 
     // Insert message
+    // if retryCount > 0, check if the message is allready in the database, not older that 6 seconds
+    if ($retryCount>0){
+		$sql="select * from messages where timestamp > DATE_SUB(NOW(),INTERVAL 6 SECOND) and regid='$regid' and message='$message'";
+		$result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            echo "0"; // Message sent successfully
+            exit();
+        }
+    }
+		
+		
     if (empty($recipientname)) {
         $sql = "INSERT INTO messages (sendername, regid, message) VALUES ('$sendername', '$regid', '$message')";
     } else {
